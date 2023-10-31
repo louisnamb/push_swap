2a36bb093e542c0762c694b95af2139717930439
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-10-31 15:14:39 +1030

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -6,62 +6,58 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:37:09 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/10/30 17:13:30 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/10/31 15:14:07 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void	check_args(int option, char **argv)
+void	check_args(int option, char **argv, int argc)
 {
-	int     i;
-	t_list  *stack_a;
+    int		i;
+    t_list	*stack_a;
+    t_list	*tmp;
 	char	**elem;
 	int		int_v;
 
-	i = -1;
-	stack_a = malloc(sizeof(t_list));
-	stack_a->next = NULL;
-	if (!stack_a)
-		return ;
-	if (option == 1)
+    i = 1;
+    stack_a = malloc(sizeof(t_list));
+    stack_a->next = NULL;
+    if (!stack_a || !ft_strlen(argv[1]))
+        return;
+    if (option == 2)
 	{
-		if (ft_strlen(*argv) > 1)
-		{
-			elem = ft_split(argv[1], ' ');
-			stack_a->content = ft_atoi(elem[0]); 
-			while (elem[++i])
-			{
-				int_v = ft_atoi(elem[i]);
-				ft_lstadd_back(&stack_a, ft_lstnew(int_v));
-			}
-		}
+		i = 0;
+		elem = ft_split(argv[1], ' ');
 	}
-	else
+    else
+        elem = argv;
+    stack_a->content = ft_atoi(elem[i++]);
+	printf("int_v: %d\n", stack_a->content);
+    while (i < argc)
 	{
-		stack_a->content = ft_atoi(argv[1]);
-		while (argv[++i])
-		{
-			int_v = ft_atoi(argv[i]);
-			ft_lstadd_back(&stack_a, ft_lstnew(int_v));
-		}
+		
+		int_v = ft_atoi(elem[i]);
+		printf("int_v: %d\n", ft_atoi(elem[i]));
+		tmp = ft_lstnew(int_v);
+		if (!tmp)// || int_v == 0 && elem[i][0] == '0'))
+			return ;
+        ft_lstadd_back(&stack_a, tmp, i);
+		tmp = NULL;
+		i++;
 	}
-	return ;
+    return ;
 }
+
 
 int main(int argc, char **argv)
 {
-	if (argc == 1)
+	if (argc <= 1 || !argv)
 	{
 		perror("Invalid arguments");
 		return (1);
 	}
 	else
-	{
-		if (argc == 2)
-			check_args(1, argv);
-		else
-			check_args(2, argv);
-	}
+		check_args((argc == 2) + 1, argv, argc);
 	return (0);
 }
