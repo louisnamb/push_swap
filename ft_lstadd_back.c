664b27efb470c6366433931cc8e3591b8bacd27f
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-11-02 13:02:10 +1030

    Add files via upload

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,18 +6,18 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 17:28:50 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/11/01 16:19:24 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/11/02 12:57:12 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h"
 #include "../inc/push_swap.h"
 
-void	ft_lstadd_back(t_list **start, t_list *new_node, int index)
+void	ft_lstadd_back(t_list **start, t_list *new_node, size_t index)
 {
-    if (!start)
+    if (!start || !new_node)
         return;
-    if ((*start)->next)
+    if (*start)
     {
         t_list *end = (*start)->prev;
         end->next = new_node;
@@ -26,11 +26,13 @@ void	ft_lstadd_back(t_list **start, t_list *new_node, int index)
         new_node->prev = end;
         (*start)->prev = new_node;
     }
-    else
-    {
-		(*start)->prev = *start;
-        (*start)->next = *start;
-        (*start)->index = index;
-        *start = new_node;
-    }
+    // else
+    // {
+	// 	new_node->prev = new_node;
+    //     new_node->next = new_node;
+    //     new_node->index = index;
+    //     *start = new_node;
+    // }
+  //  printf("back - new_node->index: %zu index: %zu\n", new_node->index, index);
+    return ;
 }
--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,36 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:37:09 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/11/01 16:18:09 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/11/02 12:59:52 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+int		printlist(t_list **stack_a, int len)
+{
+	int	i;
+	t_list	*iterator;
+
+	i = 0;
+	if (*stack_a)
+	{
+		iterator = *stack_a;
+		while (iterator && len)
+		{
+			printf("[%zu] %d\n", iterator->index, iterator->content);
+			iterator = iterator->next;
+		//	printf("here\n");
+			i++;
+			len--;
+		}
+	}
+	return (0);
+}
+
 t_list	*create_stacks(int option, char **argv)
 {
-    int		i;
+    size_t	i;
     t_list	*stack_a;
     t_list	*tmp;
 	char	**elem;
@@ -22,8 +43,8 @@ t_list	*create_stacks(int option, char **argv)
 
     i = 1;
     stack_a = malloc(sizeof(t_list));
-    stack_a->next = NULL;
-	stack_a->prev = NULL;
+    stack_a->next = stack_a;
+	stack_a->prev = stack_a;
     if (!stack_a || !ft_strlen(argv[1]))
        return (&(t_list){NULL, 0, 0, NULL});
     if (option == 2)
@@ -79,20 +100,22 @@ int main(int argc, char **argv)
 {
 	int		len;
 	t_list	*stack_a;
-	t_list	*stack_b;
+//	t_list	*stack_b;
 
+	stack_a = NULL;
 	len = error_check(argv, argc);
 	if (argc <= 1 || !argv)
 	{
 		perror("Invalid arguments");
 		exit(EXIT_FAILURE);
 	}
-	stack_b = malloc(sizeof(t_list));
+//	stack_a = malloc(sizeof(t_list));
 	if (len)
 	{
 		stack_a = create_stacks((argc == 2) + 1, argv);
 	//	sort(stack_a, stack_b, len);
 	}
+	printlist(&stack_a, len);
 	//free everything
 	return (0);
 }
