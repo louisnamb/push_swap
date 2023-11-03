7857c8a3cbd42729acf8494560b9c1a58dfea1f6
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-11-03 14:45:49 +1030

    Add files via upload

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 13:21:17 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/11/02 15:42:37 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/11/03 14:16:58 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,16 @@
 #include "../inc/libft.h"
 #include <limits.h>
 
+typedef struct  general {
+    int len;
+}   t_general;
+
 void	check_args(int option, char **argv, int argc);
-void    sort_which(t_list **stack_a, int len);
-int		printlist(t_list **stack_a, int len);
+void    sort_which(t_list **stack_a, t_list **stack_b, t_general *main);
+int		printlist(t_list **stack_a, int len, int a_or_b);
+void    swap(t_list **stack_a, t_list **stack_b, t_general *main);
+void	push(t_list **stack_a, t_list **stack_b, t_general *main);
+int     stack_len(t_list **stack);
+
 
 #endif
